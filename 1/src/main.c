afd19c401129e716d8caf33968913f5a304a18c2
SHtress <44646393+SHtress@users.noreply.github.com>
2020-11-19 00:19:01 +0300

    #1 Бутрин_Станислав+Василевский_Владислав
    * feat():Add free function, change structure block
    
    * fix(): Change size of blocks, fix delting first block
    
    * Change block, add recalculation for ptr, size
    
    * Change const size to var, remove func check
    
    * Remove useless printfs, comments, add error codes
    
    * Fix pointers
    
    * remove unused vars + rename
    
    + rename "q"->"curent_block" in mem.c
    
    * move reading block to one function
    
    * malloc and write to functions
    
    * delete unused vars in main.c
    
    * fix pointer error
    
    * add free last block
    
    Co-authored-by: v645 <45901718+v645@users.noreply.github.com>

--- 1/src/main.c
+++ 1/src/main.c
@@ -1,51 +1,87 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 #include "memory/memory.h"
 
-int main(int argc, char **argv) {
-  m_init(1, 500);
+void _read(struct block* chunk) {
+				char buffer[100];
+				int error_code;
+				m_read(chunk, buffer, chunk->size, &error_code);
+				if (error_code != M_ERR_OK) abort();
+				printf("%s\n", buffer);
+				memset(buffer, 0, sizeof(buffer));
+}
 
-  int error_code;
+struct block* _malloc(int size) {
+				int error_code;
+				struct block* newBlock = m_malloc(size, &error_code);
+				if (error_code != M_ERR_OK) abort();
+				return newBlock;
+}
 
-  m_id chunk_1 = m_malloc(13, &error_code);
-  if (error_code != M_ERR_OK) abort();
+void _write(struct block* chunk, char* data) {
+				int error_code;
+				m_write(chunk,data, chunk->size, &error_code);
+				if (error_code != M_ERR_OK) abort();
+}
 
-  m_id chunk_2 = m_malloc(20, &error_code);
-  if (error_code != M_ERR_OK) abort();
+int main(int argc, char** argv) {
+	m_init(1, 500);
 
-  m_id chunk_3 = m_malloc(50, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	int error_code;
 
-  m_write(chunk_1, "Hello World!", 13, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	struct block* chunk_1 = _malloc(23);
+	struct block* chunk_2 = _malloc(33);
+	struct block* chunk_3 = _malloc(23);
+	struct block* chunk_4 = _malloc(43);
+	struct block* chunk_5 = _malloc(23);
 
-  m_write(chunk_2, "Operating Systems", 18, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	_write(chunk_1, "Hello World!");
+	_write(chunk_2, "1234567890"); 
+	_write(chunk_3, "LAB COATS"); 
+	_write(chunk_4, "EL PSY CONGROO");
+	_write(chunk_5, "Jormungand"); 
 
-  m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
-  if (error_code != M_ERR_OK) abort();
 
-  char buffer[50];
+	printf("\n    BEFORE:    \n");
+	printf("----------FIRST CHUNK----------- \n");
+	_read(chunk_1);
 
-  m_read(chunk_1, buffer, 13, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+	printf("----------SECOND CHUNK--------- \n");
+	_read(chunk_2);
 
-  m_read(chunk_2, buffer, 18, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+	printf("---------THIRD CHUNK------- \n");
+_read(chunk_3);
 
-  m_read(chunk_3, buffer, 28, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+	printf("---------FOURTH CHUNK------- \n");
+_read(chunk_4);
 
-  m_free(chunk_1, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	printf("---------FIFTH CHUNK------- \n");
+_read(chunk_5);
 
-  m_free(chunk_2, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	//-----------------------------------------------
 
-  m_free(chunk_3, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	m_free(chunk_4, &error_code, chunk_4->size);
+
+	//-----------------------------------------------
+
+	printf("\n    AFTER:    \n");
+
+	printf("--------FIRST CHUNK--------- \n");
+	_read(chunk_1);
+ 
+	printf("----------SECOND CHUNK------\n");
+
+_read(chunk_2);
+
+	printf("---------THIRD CHUNK------- \n");
+
+_read(chunk_3);
+
+	printf("---------FOURTH CHUNK------- \n");
+
+_read(chunk_4);
+
+	printf("---------FIFTH CHUNK------- \n");
+	_read(chunk_5);
 }
